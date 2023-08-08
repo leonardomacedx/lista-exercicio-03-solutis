e45fd9e391cfeab22ad9a506f035188fe2dc914f
Leonardo Macedo <leo.depaula2019@gmail.com>
2023-08-08 20:28:45 -0300

    Questões atualizadas

--- src/main/java/leonardo/questao26/Main.java
+++ src/main/java/leonardo/questao26/Main.java
@@ -1,7 +1,6 @@
 package main.java.leonardo.questao26;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
